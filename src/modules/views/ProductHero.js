cbba425cfa7c4911c0c9c51d9c526e176f0dc557
taerinlee <taerin.lee90@gmail.com>
2022-10-12 19:23:39 +1100

    메인 사진 수정

--- src/modules/views/ProductHero.js
+++ src/modules/views/ProductHero.js
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Button from '../components/Button';
 import Typography from '../components/Typography';
 import ProductHeroLayout from './ProductHeroLayout';
 
@@ -9,7 +6,7 @@ export default function ProductHero() {
   return (
     <ProductHeroLayout
       sxBackground={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/balloon.jpg)`,
+        backgroundImage: `url(${process.env.PUBLIC_URL}/professional.jpg)`,
         backgroundColor: '#7fc7d9', // Average color of the background image.
         backgroundPosition: 'center',
       }}
@@ -17,59 +14,26 @@ export default function ProductHero() {
       {/* Increase the network loading priority of the background image. */}
       <img
         style={{ display: 'none' }}
-        src={process.env.PUBLIC_URL + '/balloon.jpg'}
+        src={process.env.PUBLIC_URL + '/professional.jpg'}
         alt="increase priority"
       />
       <Typography color="inherit" align="center" variant="h2">
         소소한 일상에, 블루밍 한방울
       </Typography>
       <Typography
+        variant="body2"
+        color="inherit"
+        sx={{ mt: 10 }}
+      >
+        소개팅 경험을 바꾸다
+      </Typography>
+      <Typography
         color="inherit"
         align="center"
         variant="h5"
-        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+        sx={{ mt: 2 }}
       >
         연애를 꿈꾸는 당신을 위한 소개팅 서비스
-      </Typography>
-      <Box
-        component="section"
-        md={{ maxWidth: 400}}
-      >
-        <Grid container>
-          <Grid item xs={6} sm={6} md={6}>
-            <Button
-              size="large"
-              component="a"
-              href="#"
-              sx={{ minWidth: 200 }}
-            >
-              <img
-                src={process.env.PUBLIC_URL + '/appstore.png'}
-                alt="appstore"
-                width="123"
-                height="42"
-              />
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={6}>
-            <Button
-              size="large"
-              component="a"
-              href="#"
-              sx={{ minWidth: 200 }}
-            >
-              <img
-                src={process.env.PUBLIC_URL + '/googleplay.png'}
-                alt="googleplay"
-                width="123"
-                height="42"
-              />
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
-      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
-        소개팅 경험을 바꾸다
       </Typography>
     </ProductHeroLayout>
   );
