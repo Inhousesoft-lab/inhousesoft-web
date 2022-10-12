ae0be9c1792e16be1043b84d18ad3d3c5260ce96
taerinlee <taerin.lee90@gmail.com>
2022-10-12 22:08:27 +1100

    링크 삭제

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,98 +18,68 @@ class Skills extends React.Component {
                   </h4>
                   <Row className="justify-content-center">
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/argon-design-system?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip255035741"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/bootstrap.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/bootstrap.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip255035741">
                         Bootstrap 4 - Most popular front-end component library
                       </UncontrolledTooltip>
                     </Col>
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/vue-argon-design-system?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip265846671"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/vue.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/vue.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip265846671">
                         Vue.js - The progressive javascript framework
                       </UncontrolledTooltip>
                     </Col>
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/argon-design-system-angular?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip233150499"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/angular.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/angular.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip233150499">
                         Angular - One framework. Mobile & Desktop
                       </UncontrolledTooltip>
                     </Col>
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip308866163"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/react.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/react.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip308866163">
                         React - A JavaScript library for building user
                         interfaces
                       </UncontrolledTooltip>
                     </Col>
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip76119384"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/sketch.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/sketch.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip76119384">
                         Sketch - Digital design toolkit
                       </UncontrolledTooltip>
                     </Col>
                     <Col lg="2" xs="4">
-                      <a
-                        href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
+                      <img
+                        alt="..."
                         id="tooltip646643508"
-                        target="_blank"
-                      >
-                        <img
-                          alt="..."
-                          className="img-fluid"
-                          src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/ps.jpg"
-                        />
-                      </a>
+                        className="img-fluid"
+                        src="https://s3.amazonaws.com/creativetim_bucket/tim_static_images/presentation-page/ps.jpg"
+                      />
                       <UncontrolledTooltip delay={0} target="tooltip646643508">
                         Adobe Photoshop - Software for digital images
                         manipulation
